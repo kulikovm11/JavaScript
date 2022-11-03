d096df886d50724a60acb293e074175da981670e
kulikovm11 <maksym.obo@gmail.com>
2022-11-03 13:30:09 +0200

    Homework N2 resolved some incorrect tasks

--- JS lesson 2/script.js
+++ JS lesson 2/script.js
@@ -37,15 +37,33 @@
 //  book1.authors = ['name','age']
 // book1.name = 'John Ronald Reuel Tolkien'
 // book1.age = 81
-//
-// book2.authors = ['name','age']
-// book1.name = 'Clive Staples Lewis'
-// book1.age = 64
-//
-// book3.authors = ['name','age']
-// book1.name = 'Jon Duckett'
-// book1.age = 40
-//
+
+// let book1 ={
+//     title: 'The Silmarillion',
+//     pageCount: 663,
+//     genre:'phantasy',
+//     authors: [{
+//         name:'John Ronald Reuel Tolkien',
+//         age:81
+//     }]};
+// let book2 = {
+//     title: 'The Magician\'s Nephew',
+//     pageCount: 181,
+//     genre:'novel',
+//     authors: [{
+//         name:'Clive Staples Lewis',
+//         age:64
+//     }]};
+// let book3 = {
+//     title: 'Javascript and jQuery',
+//     pageCount: 641,
+//     genre:'other',
+//     authors: [{
+//         name:'Jon Duckett',
+//         age:40
+//     }]}
+
+
 // console.log(book1);
 // console.log(book2);
 // console.log(book3);
@@ -53,57 +71,48 @@
 
 // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
 
-// let users = [
-//     user1={
-//         name:'Roger Lawson',
-//         username:'roger',
-//         password:'gabriel'},
-//     user2={
-//         name:'Bella Dean',
-//         username:'bella.dean',
-//         password:'macman123'},
-//     user3={
-//         name:'Amber Berry',
-//         username:'Berry',
-//         password:'47crow47'},
-//     user4={
-//         name:'Peyton Ford',
-//         username:'Peyton99',
-//         password:'11pearson6565'},
-//     user5={
-//         name:'Ida Stevens',
-//         username:'ida.stevens',
-//         password:'wheeler2812'},
-//     user6={
-//         name:'Dylan Mills',
-//         username:'Dylan01',
-//         password:'9908DM'},
-//     user7={
-//         name:'Emily Dunn',
-//         username:'EmyLee',
-//         password:'44istheman141455'},
-//     user8={
-//         name:'Gabriella Porter',
-//         username:'Gaby89',
-//         password:'alucard181'},
-//     user9={
-//         name:'Kristin Parker',
-//         username:'Kriss55',
-//         password:'fangM14'},
-//     user10={
-//         name:'Jon Caldwell',
-//         username:'Jon.Caldwell',
-//         password:'Jon161616'}
-//
-// ]
-// console.log(user1.password)
-// console.log(user2.password)
-// console.log(user3.password)
-// console.log(user4.password)
-// console.log(user5.password)
-// console.log(user6.password)
-// console.log(user7.password)
-// console.log(user8.password)
-// console.log(user9.password)
-// console.log(user10.password)
+let users = [
+    {
+        name:'Roger Lawson',
+        username:'roger',
+        password:'gabriel'},
+    {
+        name:'Bella Dean',
+        username:'bella.dean',
+        password:'macman123'},
+    {
+        name:'Amber Berry',
+        username:'Berry',
+        password:'47crow47'},
+    {
+        name:'Peyton Ford',
+        username:'Peyton99',
+        password:'11pearson6565'},
+    {
+        name:'Ida Stevens',
+        username:'ida.stevens',
+        password:'wheeler2812'},
+    {
+        name:'Dylan Mills',
+        username:'Dylan01',
+        password:'9908DM'},
+    {
+        name:'Emily Dunn',
+        username:'EmyLee',
+        password:'44istheman141455'},
+    {
+        name:'Gabriella Porter',
+        username:'Gaby89',
+        password:'alucard181'},
+    {
+        name:'Kristin Parker',
+        username:'Kriss55',
+        password:'fangM14'},
+    {
+        name:'Jon Caldwell',
+        username:'Jon.Caldwell',
+        password:'Jon161616'}
+
+]
+console.log(users[4].password)
 
